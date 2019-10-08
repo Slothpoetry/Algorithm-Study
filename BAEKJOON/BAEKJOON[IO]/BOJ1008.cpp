17e5f52d71c9c63cf8b6128c7377e81b04597d2b
Slothpoetry <blancnoir14@gmail.com>
2019-10-08 19:25:45 +0900

    [UPDATE]BOJ1008

--- BAEKJOON/BAEKJOON[IO]/BOJ1008.cpp
+++ BAEKJOON/BAEKJOON[IO]/BOJ1008.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
 	int a, b;
 	scanf("%d %d", &a, &b);
 	if (a > 0 && b < 10)
-		printf("%d", a - b);
+		printf("%.9f", (double)a/b);
+	system("pause");
 	return 0;
+
+
 }
