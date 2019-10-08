5e2c404d17a268ad028c475b0a508f467328080b
Slothpoetry <blancnoir14@gmail.com>
2019-10-08 20:03:37 +0900

    [UPDATE]BOJ10950

---
+++ BAEKJOON/BAEKJOON[IO]/BOJ10950.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, T;
+	scanf("%d",&T);
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d %d",&a,&b);
+		if(a>0 && b<10)
+			printf("%d \n" , a + b);
+	}
+	return 0;
+}
